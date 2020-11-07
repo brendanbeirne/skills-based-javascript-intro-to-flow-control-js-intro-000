8d6c66a66e483486daf4d7d0da4a15aeb3d0e45e
Brendan J Beirne <brendan.beirne@gmail.com>
2020-11-07 19:49:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,13 @@
 function basicTeenager(age) {
-  if basicTeenager (age >= 13 && =< 19)
+  if basicTeenager (age >= 13 && =< 19);
   return "You are a teenager!"
   else {
     return "You are not a teenager"
   }
+
+functio teenager(age)
+if teenager(age >= 13 && =< 19)
+return "You are not a teenager"
+else {
+  return "You are not a teenager"}
+}
