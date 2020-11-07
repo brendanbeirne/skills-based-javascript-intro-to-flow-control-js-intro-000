b8939bfeaa83a7976d5a52076d2ab85147186f0c
Brendan J Beirne <brendan.beirne@gmail.com>
2020-11-07 19:44:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,24 +4,3 @@ function basicTeenager(age) {
   else {
     return "You are not a teenager"
   }
-}
-
-function teenager(age) {
-  if true
-  return "You are a teenager!"
-  else {
-    return "You are not a teenager"
-  }
-}
-
-function ageChecker(age) {
-
-}
-
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
-}
